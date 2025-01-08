56d61d0b6479648918f73dbf2e51eafcbfcee3c7
Jurandi Junior <jurandijr20@gmail.com>
2025-01-08 20:59:20 -0300

    Modificador params

---
+++ Calculator.cs
@@ -0,0 +1,33 @@
+﻿namespace POO_CSharp_P7
+{
+    internal class Calculator
+    {
+        //Solução ruim com diversas sobrecargas:
+        /*
+        public static int Sum(int n1, int n2)
+        {
+            return n1 + n2;
+        }
+        public static int Sum(int n1, int n2, int n3)
+        {
+            return n1 + n2 + n3;
+        }
+        public static int Sum(int n1, int n2, int n3, int n4)
+        {
+            return n1 + n2 + n3 + n4;
+        }
+        */
+
+        //Solução com Vetor
+        //Utilizando modificador params
+        public static int Sum(params int[] numbers)
+        {
+            int sum = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                sum += numbers[i];
+            }
+            return sum;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,7 +4,21 @@
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Versão sem modificador
+            Console.WriteLine("Versão sem modificador");
+            int s1 = Calculator.Sum(new int[] { 2, 3 });
+            int s2 = Calculator.Sum(new int[] { 2, 4, 3 });
+
+            Console.WriteLine(s1);
+            Console.WriteLine(s2);
+            Console.WriteLine();
+
+            Console.WriteLine("Versão com o modificador Params");
+            int s3 = Calculator.Sum(2, 3);
+            int s4 = Calculator.Sum(2, 4, 3);
+            Console.WriteLine(s3);
+            Console.WriteLine(s4);
+            Console.WriteLine();
         }
     }
 }
